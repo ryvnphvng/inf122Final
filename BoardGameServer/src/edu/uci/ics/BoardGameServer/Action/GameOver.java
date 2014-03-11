1c08abc11078faed881d3f925fdb6a5c37348c6d
Daniel Hirsch <daniellancehirsch@gmail.com>
2014-03-10 21:12:55 -0700

    GameOver.java

---
+++ BoardGameServer/src/edu/uci/ics/BoardGameServer/Action/GameOver.java
@@ -0,0 +1,15 @@
+package edu.uci.ics.BoardGameServer.Action;
+
+import edu.uci.ics.BoardGameServer.Board.Board;
+
+public abstract class GameOver {
+	private Board board;
+	
+	public GameOver(Baord board)
+	{
+		this.board = board;
+	}
+	
+	public abstract Boolean isWinCondidtionMet(String s);
+	public abstract Boolean isLoseCondidtionMet(String s);
+}
