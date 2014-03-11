b857f77c23c40ecdadd52ed5a2b3e12a191d7804
Daniel Hirsch <daniellancehirsch@gmail.com>
2014-03-10 20:30:03 -0700

    MoveValidator.java

---
+++ BoardGameServer/src/edu/uci/ics/BoardGameServer/Action/MoveValidator.java
@@ -0,0 +1,14 @@
+package edu.uci.ics.BoardGameServer.Action;
+
+import edu.uci.ics.BoardGameServer.Board.Board;
+
+public abstract class MoveValidator {
+	private Board board;
+
+	public MoveValidator(Board b)
+	{
+		this.board = b;
+	}
+	
+	public abstract Boolean isValidMove(String instruction);
+}
