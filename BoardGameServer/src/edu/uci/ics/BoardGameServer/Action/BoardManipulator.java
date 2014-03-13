90c466beaacaa3fbd9b9da2ce64dc8ce9891b251
M C <mchizews@users.noreply.github.com>
2014-03-13 13:21:15 -0700

    Changes to bring code to "working" order again

--- BoardGameServer/src/edu/uci/ics/BoardGameServer/Action/BoardManipulator.java
+++ BoardGameServer/src/edu/uci/ics/BoardGameServer/Action/BoardManipulator.java
@@ -9,21 +9,21 @@ public class BoardManipulator {
 	
 	public void createGameObject(GameObject g, int objectID, String type, int playerNum, int row, int col)
 	{
-		board.addToBoard(g, objectID, type, row, col);
+		// board.addToBoard(g, objectID, type, row, col);
 	}
 	
 	public void deleteGameObject(int objectID)
 	{
-		board.removeFromBoard(objectID);
+		// board.removeFromBoard(objectID);
 	}
 	
 	public void moveGameObject(int objectID, int playerNum, int row, int col)
 	{
-		board.move(objectID, row, col);
+		// board.move(objectID, row, col);
 	}
 	
 	public void swapGameObjects(int objectID1, int objectID2)
 	{
-		board.swap(objectID1, objectID2);
+		//board.swap(objectID1, objectID2);
 	}
 }
