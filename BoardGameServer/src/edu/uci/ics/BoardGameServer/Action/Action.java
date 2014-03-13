434670a3a63a5f1c1763f8465cb4ded350e10d28
Rohan Venapusala <rohan@yayplanner.com>
2014-03-13 00:31:35 -0700

    added the ability to reciever, parse, and send JSON messages, currently only working on test strings in the action class

--- BoardGameServer/src/edu/uci/ics/BoardGameServer/Action/Action.java
+++ BoardGameServer/src/edu/uci/ics/BoardGameServer/Action/Action.java
@@ -6,8 +6,15 @@ import edu.uci.ics.BoardGameServer.Engine.Game;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Map;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 
 
@@ -30,13 +37,38 @@ public class Action {
 		this.numberOfPlayers = numberOfPlayers;
 	}
 
-	public void messageFromClient(Message message) {
+	public static void messageFromClient(String message) {
+		Object obj=JSONValue.parse(message);
+		JSONArray array=(JSONArray)obj;		
+		JSONObject gameMessage=(JSONObject)array.get(0);
+		  //to debug
+		  System.out.println(obj.toString());
+		  System.out.println("======Game Messages==========");
+		  System.out.println("MessageType :" + gameMessage.get("MessageType")); 
+		  System.out.println("GameID :" + gameMessage.get("GameID"));
+		  System.out.println("PlayerID :" + gameMessage.get("PlayerID"));
+		  System.out.println("ObjectID :" + gameMessage.get("ObjectID"));
+		  System.out.println("Row :" + gameMessage.get("Row"));
+		  System.out.println("Col :" + gameMessage.get("Col"));
+		  System.out.println("Message :" + gameMessage.get("Message"));
+		  
+	}
+	
+	public static String messageToClient() {
+		
+		String message = "[{\"MessageType\":\"startGame\" , \"GameID\":\"25\", \"PlayerID\":\"25\", \"ObjectID\":\"25\",\"Row\":\"0\",\"Col\":\"1\",\"Message\":\"Place\" }]";
+		return message;
 	}
 
-	private void messageToClient(Message message) {
-		message.message = "blah";
-		message.playerNumber = 0;
-		game.messageToClient(message);
+	//for testing purposes only!
+	public static void main(String [ ] args)
+	{
+		String Message="[{\"MessageType\":\"startGame\" , \"GameID\":\"25\", \"PlayerID\":\"25\", \"ObjectID\":\"25\",\"Row\":\"0\",\"Col\":\"1\",\"Message\":\"Place\" }]";
+		messageFromClient(Message);
+		
 	}
+
+	
+	
 
 }
