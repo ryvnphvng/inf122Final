96ee1b4624b52d5fa0a5002b7ebef50555a95cad
Daniel Hirsch <dlhirsch@users.noreply.github.com>
2014-03-15 23:20:45 -0700

    Minor fix in TicTacToeValidator.java

--- BoardGameServer/src/edu/uci/ics/BoardGameServer/Action/TicTacToeValidator.java
+++ BoardGameServer/src/edu/uci/ics/BoardGameServer/Action/TicTacToeValidator.java
@@ -13,17 +13,22 @@ public class TicTacToeValidator extends MoveValidator {
 	}
 
 	public Boolean isValidMove(JSONObject gameMessage) {
-		int row = Integer.parseInt(gameMessage.get("Row").toString());
-		int col = Integer.parseInt(gameMessage.get("Col").toString());
+		if(gameMessage.get("MessageType").equals("Create"))
+		{
+			int row = Integer.parseInt(gameMessage.get("Row").toString());
+			int col = Integer.parseInt(gameMessage.get("Col").toString());
+			
+			if(board.getTile(row, col).getGameObjects().size() > 0)
+			{
+				return false; // A player has already put an 'X' or 'O' in this tile. Move is invalid.
+			}
+			else
+			{
+				return true; // A play has not put an 'X' or 'O' in this tile. Move is valid.
+			}
+		}
 		
-		if(board.getTile(row, col).getGameObjects().size() > 0)
-		{
-			return false; // A player has already put an 'X' or 'O' in this tile. Move is invalid.
-		}
-		else
-		{
-			return true; // A play has not put an 'X' or 'O' in this tile. Move is valid.
-		}
+		return false;
 	}
 
 }
