9686ed778605180ed1458363554da727b4ec98b8
M C <mchizews@users.noreply.github.com>
2014-03-16 21:32:25 -0700

    Updated uml

--- BoardGameServer/src/edu/uci/ics/BoardGameServer/Distribution/ClientConnection.java
+++ BoardGameServer/src/edu/uci/ics/BoardGameServer/Distribution/ClientConnection.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-//Temporary class
-
 public class ClientConnection {
 	public boolean alive = true;
 	public long lastMessagetime = System.currentTimeMillis();
