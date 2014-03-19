88bd572a4f3f658535a3d37360e8633f6c3cc038
Maksim Zhilin <mzhilin@uci.edu>
2014-03-19 15:33:45 -0700

    Add server definitions for connect 4 parts

--- BoardGameServer/src/edu/uci/ics/BoardGameServer/Board/GameObjectDefinitions.java
+++ BoardGameServer/src/edu/uci/ics/BoardGameServer/Board/GameObjectDefinitions.java
@@ -4,6 +4,9 @@ public class GameObjectDefinitions {
 	public static final int TICTACTOE_X = 0;
 	public static final int TICTACTOE_O = 1;
 	
+	public static final int CONNECT4_RED = 2;
+	public static final int CONNECT4_YELLOW = 3;
+	
 	private GameObjectDefinitions() {
 	}
 
--- BoardGameServer/src/edu/uci/ics/BoardGameServer/Common/Definitions.java
+++ BoardGameServer/src/edu/uci/ics/BoardGameServer/Common/Definitions.java
@@ -3,6 +3,8 @@ package edu.uci.ics.BoardGameServer.Common;
 public class Definitions {
 
 	public static final int GAMETYPETICTACTOE = 0;
+	public static final int GAMETYPECONNECTFOUR = 1;
+	public static final int GAMETYPEMATCHING = 2;
 
 	private Definitions() {
 	}
