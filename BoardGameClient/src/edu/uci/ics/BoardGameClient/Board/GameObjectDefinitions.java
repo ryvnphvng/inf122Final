c04dbac36f60091877f9e18be03a5c99d463dfd0
Maksim Zhilin <mzhilin@uci.edu>
2014-03-19 15:51:38 -0700

    Update client gameobjectdefinitions for connect4

--- BoardGameClient/src/edu/uci/ics/BoardGameClient/Board/GameObjectDefinitions.java
+++ BoardGameClient/src/edu/uci/ics/BoardGameClient/Board/GameObjectDefinitions.java
@@ -4,8 +4,8 @@ public class GameObjectDefinitions {
 	public static final int TICTACTOE_X = 0;
 	public static final int TICTACTOE_O = 1;
 	
-	public static final int CONNECT4_RED = 0;
-	public static final int CONNECT4_YELLOW = 1;
+	public static final int CONNECT4_RED = 2;
+	public static final int CONNECT4_YELLOW = 3;
 	
 	private GameObjectDefinitions() {
 	}
